5d809133b942f81b6affc364208e6ea89e6d0216
ynurr <74530798+ynurr@users.noreply.github.com>
2023-07-13 23:36:01 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 916 ms, Memory: 237608 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int sum = a+b;
+            
+            bw.write(sum+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
