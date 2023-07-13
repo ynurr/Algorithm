885883a90a00d53c0caeae75d7a964c00b07e796
ynurr <74530798+ynurr@users.noreply.github.com>
2023-07-13 23:41:23 +0900

    [Bronze II] Title: 나머지, Time: 228 ms, Memory: 18268 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.HashSet;
+ 
+public class Main {
+	public static void main(String[] args) {
+	
+        Scanner sc = new Scanner(System.in);
+ 
+        HashSet<Integer> h = new HashSet<Integer>();
+        
+        for(int i = 0; i <10; i++) {
+            h.add(sc.nextInt() % 42);
+        } // 입력 받은 값의 나머지 값을 add 메소드를 통해 HashSet에 저장
+        
+        sc.close();
+        
+        System.out.print(h.size());
+    }
+}
