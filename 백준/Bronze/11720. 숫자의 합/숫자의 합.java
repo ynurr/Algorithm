108af830e5858a55620d125625d04d6ad0270f77
ynurr <74530798+ynurr@users.noreply.github.com>
2023-07-17 23:52:24 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -17,3 +17,27 @@ public class Main {
         sc.close();
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String a = sc.next();
+        int result = 0;
+
+        for(int i = 0; i < n; i++) {
+            result += a.charAt(i)-'0'; 
+            // charAt(0)부터 마지막 charAt(n)까지 누적 합 구하기
+            // charAt()은 해당 문자의 아스키코드 값을 반환하므로 반드시 -48 또는 -'0' 을 해줘야 입력받은 숫자 값 그대로를 사용할 수 있다.
+        }
+        
+        System.out.println(result);
+
+        sc.close();
+    }
+}
+*/
