7b6fac2910405a75c9b36890fa761e9165596912
ynurr <74530798+ynurr@users.noreply.github.com>
2023-07-31 22:11:17 +0900

    [Silver V] Title: 색종이, Time: 220 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[][] arr = new int[100][100];
+        int n = sc.nextInt(); // 색종이의 수
+        int result = 0;
+
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for(int j = a; j < a+10; j++) { // 크기가 10인 정사각형
+                for(int k = b; k < b+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < 100; i++) {
+            for(int j = 0; j < 100; j++) {
+                if(arr[i][j] == 1) {};
+                result += arr[i][j];
+            }
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
