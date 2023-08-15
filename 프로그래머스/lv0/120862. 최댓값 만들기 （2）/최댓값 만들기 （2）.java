9a624f8b187e09837d6a469f88a8e36fdc9688df
ynurr <74530798+ynurr@users.noreply.github.com>
2023-08-16 00:51:42 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = numbers[0] * numbers[1];
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                if(numbers[i] * numbers[j] > answer) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return answer;
+    }
+}
