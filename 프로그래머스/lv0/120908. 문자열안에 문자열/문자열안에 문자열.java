04104e41bcef850eb3e653db3e9aaefecd71340b
ynurr <74530798+ynurr@users.noreply.github.com>
2023-08-16 00:47:43 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        answer = (str1.indexOf(str2) == -1) ? 2 : 1;
+        
+        return answer;
+    }
+}
